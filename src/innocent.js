412cda9e702aa1a751c01d15eb4ca42577e9a0b1
firejoust <42525540+firejoust@users.noreply.github.com>
2023-04-08 11:33:45 +1000

    use default heuristics

--- src/innocent.js
+++ src/innocent.js
@@ -1,4 +1,15 @@
 module.exports.inject = function inject(bot, SwitchMode) {
+    const MovementGoal = new bot.movement.Goal({
+        'proximity': bot.movement.heuristic.new("proximity")
+            .configure({}),
+        'conformity': bot.movement.heuristic.new("conformity")
+            .configure({}),
+        'danger': bot.movement.heuristic.new("danger")
+            .configure({}),
+        'distance': bot.movement.heuristic.new("distance")
+            .configure({})
+    })
+
     return class Innocent {
         register() {
             bot.on("message", this.message)
@@ -11,99 +22,27 @@ module.exports.inject = function inject(bot, SwitchMode) {
         }
 
         init() {
-            bot.on("physicsTick", this.tick)
-
-            // register objective heuristics
-            {
-                bot.movement.heuristic.register('proximity', 'gold')
-                    .weight(1)
-
-                bot.movement.heuristic.register('proximity', 'murderer')
-                    .weight(0)
-                    .avoid(true)
-            }
-
-            // register terrain navigation heuristics
-            {
-                bot.movement.heuristic.register('distance', 'long_distance')
-                    .weight(0.5)
-                    .radius(20)
-                    .count(5)
-                    .spread(25)
-                    .increment(0.5)
-                    .offset(1)
-
-                bot.movement.heuristic.register('distance', 'short_distance')
-                    .weight(0.45)
-                    .radius(4)
-                    .count(3)
-                    .spread(30)
-                    .offset(1)
-
-                bot.movement.heuristic.register('danger', 'descent')
-                    .weight(0.35)
-                    .radius(6)
-                    .depth(5)
-                    .count(12)
-                    .descend(true)
-
-                bot.movement.heuristic.register('danger', 'holes')
-                    .weight(0.65)
-                    .radius(3)
-                    .depth(2)
-                    .count(6)
-
-                bot.movement.heuristic.register('conformity')
-                    .weight(0.3)
-            }
-
-            // randomise periods where the player will stop
-            this.timeout = null
-            this.wait = false
-            this.waitTicks = 0
-
+            bot.movement.setGoal(MovementGoal)
             bot.setControlState("forward", true)
+            bot.on("physicsTick", this.tick)
         }
 
         stop() {
             bot.off("physicsTick", this.tick)
             bot.clearControlStates()
-            clearTimeout(this.timeout)
         }
 
         tick = () => {
             bot.clearControlStates()
 
-            // randomise "stopping" periods where movement is ceased
-            {
-                // verify no timeouts are currently pending
-                if (!this.wait) {
-                    const duration = (15 * 1000) + (Math.floor(10 * Math.random()) * 1000)
-                    const ticks = 20 + Math.floor(Math.random() * 40)
-                    this.wait = true
-
-                    // set a duration from now where the player will stop moving
-                    this.timeout = setTimeout(() => {
-                        this.waitTicks = ticks
-                        this.wait = false
-                    }, duration)
-                }
-
-                // ticks applied by timeout; freeze movement until it has reached 0
-                if (this.waitTicks > 0) {
-                    this.waitTicks--
-                    return
-                }
-            }
-
             // if gold is nearby, change the heuristic
             {
                 const entity = bot.nearestEntity(entity => 
                     entity.mobType === "Dropped item" &&
-                    entity.position.distanceTo(bot.entity.position) < 3
+                    Math.abs(entity.position.y - bot.entity.position.y) < 3
                 )
 
-                bot.movement.heuristic.get('gold')
+                bot.movement.heuristic.get('proximity')
                     .target(entity?.position || null)
             }
 
@@ -144,7 +83,7 @@ module.exports.inject = function inject(bot, SwitchMode) {
 
             // only chnage direction on the ground or in water
             if (bot.entity.onGround || bot.entity.isInWater) {
-                const yaw = bot.movement.getYaw(220, 25, 3)
+                const yaw = bot.movement.getYaw(120, 25, 0)
                 bot.movement.steer(yaw, false)
             }
 
